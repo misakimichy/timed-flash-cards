5733b5807201fc55c36b305d1323673a4baede89
Misaki Koonce and Vera Weikel <vera@>
2019-09-10 15:04:18 -0700

    Use .text instead of prepend.

--- src/main.js
+++ src/main.js
@@ -4,48 +4,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import FlashCard from "./theFlashCard.js";
 
-// ToDo: Add onclick function for form
 const clearInput = () => {
   $("input[name=card]:checked").prop("checked", false);
-  $("form#card").find('h2:first-child').remove();
 };
 
 $(document).ready(function() {
   
   const newCard = new FlashCard();
-  let cardIndex = 0;
-  const showCard = () => {
-    const cards = Object.keys(newCard.questions).map(card => {
-      return card;
-    });
-    if(cardIndex < cards.length) {
-      return cards[cardIndex];
-    } else {
-      return false;
-    }
-  };
-
   let questionIndex = 0;
+  let userAnsArr = [];
   const showQuestion = () => {
     const questions = newCard.setDisplay();
     if (questionIndex < questions.length){
-      $("form#card").prepend(`<h2>${questions[questionIndex]}</h2>`);
+      $("#question").text(`${questions[questionIndex]}`);
       return ;
     } else {
       alert("No more cards!");
-      $("#card").empty();
+      $("#card").hide();
+      $("#result").text(newCard.checkAnswer(userAnsArr));
+      $("#result").show();
     }
   };
   showQuestion();
+
   $("form").submit(function(event){
     event.preventDefault();
-    clearInput();
-    
     let answerVal = parseInt($("input[name=card]:checked").val());
-    console.log(newCard.checkAnswer(answerVal, showCard()));
-    
+    userAnsArr.push(answerVal);
     questionIndex++;
-    cardIndex++;
     showQuestion();
+    clearInput();
   });
 });
