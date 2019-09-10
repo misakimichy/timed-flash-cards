9f33eb33d5e3b28d6b993ee8be67705a992ca769
Misaki Koonce and Vera Weikel <misaki@>
2019-09-10 09:42:29 -0700

    Rename the file.

---
+++ spec/flash-cards-spec.js
@@ -0,0 +1,42 @@
+import FlashCard from '../src/theFlashCard.js';
+
+// - Spec:  Flash card questions will appear to user one question at a time.
+// Input: N/A  
+// Output: N/A
+describe('Testing the display of the question', function() {
+  const newCard = new FlashCard(0);
+  it('should return a question', function() {
+    expect(newCard.setDisplay()[0]).toEqual("The complement to the object.(?):pair is 'key'.");
+  });
+// - Spec: Capture a chosen value for user answer. 
+// Input: T
+// Output: T
+  it('should return setDisplay', function() {
+    expect(newCard.checkAnswer(answerVal)).toEqual(null);
+  });
+
+// - Spec: If user does not select an answer, display message "Please select an answer". 
+// Input: N/A
+// Output: "Please select an answer"
+
+// - Spec: If user chooses a correct answer, increment points.
+// Input: T (correct answer)
+// Output: (+1)
+
+// - Spec: If user chooses a incorrect answer, decrement points.
+// Input: T (incorrect answer)
+// Output: (-1)
+
+// - Spec: If user does not select an answer, display message "About to timeout/lose point! Select quickly". 
+// Input: N/A
+// Output: "About to timeout/lose point! Select quickly"
+
+// - Spec: Question will timeout within a given time frame and user will automatically have points deducted.
+// Input: N/A
+// Output: (-1)
+
+// - Spec:  Display user score when last flash card question has been answered.
+// Input: User selects final answer. 
+// Output: "Final Score: 10!"
+});
+
